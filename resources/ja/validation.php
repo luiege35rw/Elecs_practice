b2701834b73849b63d22e3a4125af3a85e101a7e
Fujita Namiko <fujita-namiko@elecs-web.co.jp>
2022-05-16 09:44:34 +0900

    お問い合わせフォーム仮1

---
+++ resources/ja/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+
+'email' => ':attributeが正しくありません。',
+'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+'numeric' => ':attributeは数字で入力してください。',
+'required' => ':attributeは必須項目です。',
+
+'attributes' => [
+        'name'     => 'お名前',
+        'email'    => 'メールアドレス',
+        'tel'      => '電話番号',
+        'gender'   => '性別',
+        'contents' => 'お問い合わせ内容',
+    ],
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,59 +1,68 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>お問い合わせ</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
-<body>
+@php
+$title = 'お問い合わせ';
+@endphp
+
+@extends('layout.master')
+
+@section('content')
+    <h1 class="text-center mt-2 mb-5">お問い合わせ</h1>
     <div class="container">
-        <h1 class="text-center mt-2 mb-5">お問い合わせ</h1>
-        <div class="container">
-            {!! Form::open(['route' => 'contact.confirm', 'method' => 'POST']) !!}
-                {{ csrf_field() }}
-                <div class="form-group row">
-                    <p class="col-sm-4 col-form-label">お名前（10文字以内）<span class="badge badge-danger ml-1">必須</span></p>
-                    <div class="col-sm-8">
-                        {{ Form::text('name', null, ['class' => 'form-control']) }}
-                    </div>
+        {!! Form::open(['route' => 'contact.confirm', 'method' => 'POST']) !!}
+            {{ csrf_field() }}
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">お名前（10文字以内）<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::text('name', null, ['class' => 'form-control']) }}
                 </div>
+            </div>
+            @if ($errors->has('name'))
+                <p class="alert alert-danger">{{ $errors->first('name') }}</p>
+            @endif
 
-                <div class="form-group row">
-                    <p class="col-sm-4 col-form-label">メールアドレス<span class="badge badge-danger ml-1">必須</span></p>
-                    <div class="col-sm-8">
-                        {{ Form::text('email', null, ['class' => 'form-control']) }}
-                    </div>
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">メールアドレス<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::text('email', null, ['class' => 'form-control']) }}
                 </div>
+            </div>
+            @if ($errors->has('email'))
+                <p class="alert alert-danger">{{ $errors->first('email') }}</p>
+            @endif
 
-                <div class="form-group row">
-                    <p class="col-sm-4 col-form-label">電話番号</p>
-                    <div class="col-sm-8">
-                        {{ Form::text('tel', null, ['class' => 'form-control']) }}
-                    </div>
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">電話番号</p>
+                <div class="col-sm-8">
+                    {{ Form::text('tel', null, ['class' => 'form-control']) }}
                 </div>
+            </div>
+            @if ($errors->has('tel'))
+                <p class="alert alert-danger">{{ $errors->first('tel') }}</p>
+            @endif
 
-                <div class="form-group row">
-                    <p class="col-sm-4 col-form-label">性別<span class="badge badge-danger ml-1">必須</span></p>
-                    <div class="col-sm-8">
-                        <label>{{ Form::radio('gender', "男性") }}男性</label>
-                        <label>{{ Form::radio('gender', "女性") }}女性</label>
-                    </div>
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">性別<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    <label>{{ Form::radio('gender', "男性") }}男性</label>
+                    <label>{{ Form::radio('gender', "女性") }}女性</label>
                 </div>
+            </div>
+            @if ($errors->has('gender'))
+                <p class="alert alert-danger">{{ $errors->first('gender') }}</p>
+            @endif
 
-                <div class="form-group row">
-                    <p class="col-sm-4 col-form-label">お問い合わせ内容<span class="badge badge-danger ml-1">必須</span></p>
-                    <div class="col-sm-8">
-                        {{ Form::textarea('contents', null, ['class' => 'form-control']) }}
-                    </div>
+            <div class="form-group row">
+                <p class="col-sm-4 col-form-label">お問い合わせ内容<span class="badge badge-danger ml-1">必須</span></p>
+                <div class="col-sm-8">
+                    {{ Form::textarea('contents', null, ['class' => 'form-control']) }}
                 </div>
-                
-                <div class="text-center">
-                    {{ Form::submit('確認画面へ', ['class' => 'btn btn-primary']) }}
-                </div>
-            {!! Form::close() !!}
-        </div>
+            </div>
+            @if ($errors->has('contents'))
+                <p class="alert alert-danger">{{ $errors->first('contents') }}</p>
+            @endif
+            
+            <div class="text-center">
+                {{ Form::submit('確認画面へ', ['class' => 'btn btn-primary']) }}
+            </div>
+        {!! Form::close() !!}
     </div>
-</body>
-</html>
+@endsection
